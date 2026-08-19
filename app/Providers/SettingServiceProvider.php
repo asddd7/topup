28808ccef55326c9df3df7c1e04e3bd073ef8be9
asddd7 <adipatisuryadimyati@gmail.com>
2026-08-19 11:10:04 +0700

    fix railway deployment configuration

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -9,17 +9,11 @@ use Illuminate\Support\ServiceProvider;
 
 class SettingServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
         $settings = Cache::rememberForever(
