2ea016ba8c5267c02cc3a8feb80de557da85c090
asddd7 <adipatisuryadimyati@gmail.com>
2026-09-03 13:56:54 +0700

    feat: Force HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\Notification;
@@ -73,5 +74,13 @@ $notificationCount = $notifications->count();
 
         }
     );
+
+            if (config('app.env') === 'production') {
+
+                    URL::forceScheme('https');
+
+                }
     }
+
+    
 }
